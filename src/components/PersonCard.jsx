6d58128e2b6eeeefde05ac7d4864bb54d02b0f6c
Wendi Li <issacbbb@hotmaill.com>
2022-03-09 20:52:56 -0800

    initial commit

---
+++ src/components/PersonCard.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export class PersonCard extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            age: props.age
+        }
+    }
+    render() {
+        const {firstName,lastName,hairColor} = this.props
+        return (
+            <div>
+                <h1>{firstName} , {lastName} </h1>
+                <p>Age: {this.state.age}</p>
+                <p>Hair Color: {hairColor}</p>
+                <button onClick={()=>this.setState({age: this.state.age + 1})}>birthday button for {firstName}  {lastName} </button>
+            </div>
+        )
+    }
+}
+
+export default PersonCard
